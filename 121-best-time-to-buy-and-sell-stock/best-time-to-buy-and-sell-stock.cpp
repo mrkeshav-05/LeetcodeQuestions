e3258eecee5d1183ff618bfee254ab2b2deed234
Kehsav Pundhir <145784447+mrkeshav-05@users.noreply.github.com>
2026-05-21 00:38:17 +0530

    Time: 191 ms (6.61%) | Memory: 167.5 MB (5.11%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,37 +1,29 @@
 class Solution {
 public:
-    int fun(int index, int min_price_so_far, vector<int>& prices, vector<int>& dp) {
-        // Base Case: Out of days, no more profit can be made
-        if (index == prices.size()) {
+    int fun(int idx, int canBuy, vector<int>& prices, vector<vector<int>> &dp) {
+        if(idx == prices.size()){
             return 0;
         }
-
-        // Memoization Check: If we have already calculated the max profit 
-        // starting from this day, return it.
-        if (dp[index] != -1) {
-            return dp[index];
+        int max_profit = 0;
+        if(dp[idx][canBuy] != -1){
+            return dp[idx][canBuy];
+        }
+        if(canBuy == 1){
+            int buy_today = -prices[idx] + fun(idx+1, 0, prices, dp);
+            int skip_today = 0 + fun(idx+1, 1, prices, dp);
+            max_profit = max(buy_today, skip_today);
+        }else{
+            int sell_today = prices[idx];
+            int skip_today = 0 + fun(idx + 1, 0, prices, dp);
+            max_profit = max(sell_today, skip_today);
         }
 
-        // 1. Calculate potential profit if we sold everything today
-        int current_profit = prices[index] - min_price_so_far;
-
-        // 2. Update the minimum price seen so far for the next days
-        int next_min_price = min(min_price_so_far, prices[index]);
-
-        // 3. Recurse to find the maximum profit available in the future days
-        int future_profit = fun(index + 1, next_min_price, prices, dp);
-
-        // Save and return the maximum of today's profit vs any future profit
-        return dp[index] = max(current_profit, future_profit);
+        return dp[idx][canBuy] = max_profit;
     }
 
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        
-        // dp[i] will store the maximum profit achievable from day i onwards
-        vector<int> dp(n, -1);
-        
-        // Start from day 0, with the initial minimum price set to infinity (INT_MAX)
-        return fun(0, INT_MAX, prices, dp);
+        vector<vector<int>> dp(n, vector<int> (2, -1));
+        return fun(0, 1, prices, dp);
     }
 };
