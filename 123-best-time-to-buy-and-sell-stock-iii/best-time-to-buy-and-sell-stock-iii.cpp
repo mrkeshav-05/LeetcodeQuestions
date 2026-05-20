afdfee4db66af028a7fdc6c97b17dac3d2288e11
Kehsav Pundhir <145784447+mrkeshav-05@users.noreply.github.com>
2026-05-21 02:06:54 +0530

    Time: 458 ms (18.00%) | Memory: 224 MB (15.95%) - LeetSync

---
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int fun(vector<int>& prices, int n, int idx, int number_of_trans_left, int canbuy, vector<vector<vector<int>>> &dp){
+        if(idx == n){
+            return 0;
+        }
+        if(dp[idx][canbuy][number_of_trans_left] != -1){
+            return dp[idx][canbuy][number_of_trans_left];
+        }
+        int profit = 0;
+        if(number_of_trans_left > 0){
+            if(canbuy){
+                int buy_today = -prices[idx] + fun(prices, n, idx+1, number_of_trans_left, 0, dp);
+                int skip_today = 0 + fun(prices, n, idx+1, number_of_trans_left, 1, dp);
+
+                profit = max(buy_today, skip_today);
+            }else{
+                int sell_today = prices[idx] + fun(prices, n, idx+1, number_of_trans_left-1, 1, dp);
+                int skip_today = 0 + fun(prices, n, idx+1, number_of_trans_left, 0, dp);
+
+                profit = max(sell_today, skip_today);
+            }
+        }
+        return dp[idx][canbuy][number_of_trans_left] = profit;
+    }
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(3, -1)));
+        return fun(prices, n, 0, 2, 1, dp);
+    }
+};
