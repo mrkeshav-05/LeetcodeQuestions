fe142342b0af9279a1df76fea3ab3273c4996a53
Kehsav Pundhir <145784447+mrkeshav-05@users.noreply.github.com>
2026-05-23 19:21:27 +0530

    Time: 5 ms (70.94%) | Memory: 15.4 MB (47.15%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
-    int fun(vector<int>& nums, int target, int idx){
+    int fun(vector<int>& nums, int target, int idx, vector<vector<int>> &dp){
         if(idx < 0) {
             return target == 0 ? 1 : 0;
         }
+        if(dp[idx][target] != -1){
+            return dp[idx][target];
+        }
         int take = 0;
         if(target >= nums[idx]){
-            take = fun(nums, target - nums[idx], idx-1);
+            take = fun(nums, target - nums[idx], idx-1, dp);
         }
-        int nottake = fun(nums, target, idx-1);
+        int nottake = fun(nums, target, idx-1, dp);
 
-        return take + nottake;
+        return dp[idx][target] = take + nottake;
 
     }
     int findTargetSumWays(vector<int>& nums, int target) {
@@ -19,11 +22,12 @@ public:
         if((sum + target) % 2 != 0){
             return 0;
         }
-        if(target > sum){
+        if(abs(target) > sum){
             return 0;
         }
         int newtarget = (sum + target)/2;
+        vector<vector<int>> dp(n, vector<int>(newtarget + 1, -1));
 
-        return fun(nums, newtarget, n-1);
+        return fun(nums, newtarget, n-1, dp);
     }
 };
