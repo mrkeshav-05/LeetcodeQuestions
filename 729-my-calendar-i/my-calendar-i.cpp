125f336eeb8da5695877909a76f1871bf6f5b54a
Kehsav Pundhir <145784447+mrkeshav-05@users.noreply.github.com>
2026-06-17 04:45:54 +0530

    Time: 19 ms (56.86%) | Memory: 43 MB (73.91%) - LeetSync

--- 729-my-calendar-i/my-calendar-i.cpp
+++ 729-my-calendar-i/my-calendar-i.cpp
@@ -1,34 +1,15 @@
 class MyCalendar {
+private:
+    vector<pair<int, int>> calendar;
+
 public:
-    vector<vector<int>> meetings;
-    MyCalendar() {
-        
-    }
-    
-    bool book(int startTime, int endTime) {
-        int n = meetings.size();
-        if(n == 0){
-            vector<int> temp;
-            temp.push_back(startTime);
-            temp.push_back(endTime);
-            meetings.push_back(temp);
-            return true;
-        }
-        for(auto meeting : meetings){
-            if((startTime >= meeting[0] && startTime < meeting[1]) || (endTime > meeting[0] && endTime <= meeting[1]) || (endTime > meeting[1] && startTime <= meeting[0])){
+    bool book(int start, int end) {
+        for (const auto [s, e] : calendar) {
+            if (start < e && s < end) {
                 return false;
             }
         }
-        vector<int> temp;
-        temp.push_back(startTime);
-        temp.push_back(endTime);
-        meetings.push_back(temp);
+        calendar.emplace_back(start, end);
         return true;
     }
 };
-
-/**
- * Your MyCalendar object will be instantiated and called as such:
- * MyCalendar* obj = new MyCalendar();
- * bool param_1 = obj->book(startTime,endTime);
- */
